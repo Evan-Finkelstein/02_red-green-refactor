14314eb74d7c15d08ba68a3eee47a8059a278efb
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-12-01 14:30:09 -0800

    filter and capital with for loop

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ const copyAndPush = (arr, num) => {
 
 };
 
+const capitalizeAndFilter = (arr) => {
+    const newArr = []
+    const filtered = arr.filter(name => !name.startsWith('f'));
+    for (let i = 0; i < filtered.length; i++) {
+
+        newArr.push(filtered[i].toUpperCase())
+
+
+    }
+    return newArr
+
+};
+
 
 module.exports = {
     getName,
     copyAndPush,
+    capitalizeAndFilter,
 }
 
 
--- index.test.js
+++ index.test.js
@@ -1,8 +1,12 @@
-const { getName, copyAndPush } = require('./index');
+const { getName,
+    copyAndPush,
+    capitalizeAndFilter } = require('./index');
 const spot = { name: 'spot', age: 5, weight: '20 lbs' }
 const numbers = [1, 2, 3]
-
+const dogNames = ['spot', 'rover', 'bingo', 'fred']
 test('get name', () => expect(getName(spot)).toEqual('spot'));
 
 test('copy and push', () => expect(copyAndPush(numbers, 4)).toEqual([1, 2, 3, 4]));
 test('copy and push original arr', () => expect(numbers).toEqual([1, 2, 3]));
+
+test('capitilize and filter', () => expect(capitalizeAndFilter(dogNames)).toEqual(['SPOT', 'ROVER', 'BINGO']));
